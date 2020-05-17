a3fe867f8a77cb5107ac5affe5db4d91e277dd36
Keks <keks@htmlacademy.ru>
2020-05-17 23:37:16 +0300

    :heavy_check_mark: Сборка #4

--- 4/js/script.js
+++ 4/js/script.js
@@ -10474,27 +10474,16 @@ __webpack_require__.r(__webpack_exports__);
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony default export */ __webpack_exports__["default"] = (() => {
-  window.onload = function () {
-    const $rules = document.querySelector(`.rules`);
-    const $rulesCollection = $rules.querySelectorAll(`.rules__item`);
-    const $lastRuleElement = $rulesCollection[$rulesCollection.length - 1];
-    const $LastRuleElementText = $lastRuleElement.querySelector(`p`);
-    const $rulesLink = $rules.querySelector(`.rules__link`);
-    const $rulesLinkText = $rulesLink.querySelector(`span`);
+  const $rules = document.querySelector(`.rules`);
+  const $rulesCollection = $rules.querySelectorAll(`.rules__item`);
+  const $lastRuleElement = $rulesCollection[$rulesCollection.length - 1];
+  const $LastRuleElementText = $lastRuleElement.querySelector(`p`);
+  const $rulesLink = $rules.querySelector(`.rules__link`);
 
-    $LastRuleElementText.onanimationend = (evt) => {
-      if (evt.animationName === `fadeIn`) {
-        $rulesLink.style.animationName = `animationBtn`;
-        $rulesLink.style.animationFillMode = `forwards`;
-        $rulesLink.style.animationDuration = `0.35s`;
-        $rulesLink.style.overflow = `visible`;
-
-        $rulesLinkText.style.animationName = `fadeIn`;
-        $rulesLinkText.style.animationDuration = `0.35s`;
-        $rulesLinkText.style.animationDelay = `0.2s`;
-        $rulesLinkText.style.animationFillMode = `forwards`;
-      }
-    };
+  $LastRuleElementText.onanimationend = (evt) => {
+    if (evt.animationName === `fadeIn`) {
+      $rulesLink.classList.add(`rules__link--animated`);
+    }
   };
 });
 
